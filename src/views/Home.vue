e48942805965016de4691f67fae2bd492328acbc
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-11-06 18:38:44 +0200

    add license

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,71 @@
 <template>
-<div>
-  <div class="home">
-  </div>
-  <div class="footer">
-    <hr/>
-    <div class="d-flex margins-creator">
-      <b-button variant="danger" class="mr-auto">cancel</b-button>
-      <b-button variant="primary">next</b-button>
+  <div>
+    <div class="home">
+      <h1>License Agreement</h1>
+      <b-form-textarea
+        id="textarea"
+        v-model="licenseText"
+        placeholder="Enter something..."
+        rows="14"
+        max-rows="14"
+        readonly
+      ></b-form-textarea>
+    </div>
+    <div class="footer">
+      <hr />
+      <div class="d-flex margins-creator">
+        <b-button variant="danger" class="mr-auto">cancel</b-button>
+        <b-button variant="primary">next</b-button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'home'
-}
+  name: "home",
+  data: function() {
+    return {
+      licenseText: `Copyright (c) 2010-2015 Illumina, Inc.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIEDi
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+`
+    };
+  }
+};
 </script>
 <style>
 .footer {
-    position: fixed;
-    bottom: 0;
-    width: 100%;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 }
 .margins-creator {
-  margin: 0 15px 15px 15px
+  margin: 0 15px 15px 15px;
+}
+.home {
+  margin: 10px;
 }
 </style>
