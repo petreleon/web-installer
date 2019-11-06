20ca1b655f7776bf54cb008390aefb5b01aaebff
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-11-06 15:00:15 +0200

    add buttons for cancel and next

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
+<div>
   <div class="home">
   </div>
+  <div class="footer">
+    <hr/>
+    <div class="d-flex">
+      <b-button variant="danger" class="mr-auto">cancel</b-button>
+      <b-button variant="primary">next</b-button>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -10,3 +19,10 @@ export default {
   name: 'home'
 }
 </script>
+<style>
+.footer {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+</style>
