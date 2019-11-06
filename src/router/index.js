2868d5ed752725d8c12ac0e92fb51191b658060f
Petre Leonard Macamete <petreleonardos@gmail.com>
2019-11-06 21:22:23 +0200

    Create options folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Folder from '../views/Folder.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,10 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  }, {
+    path: '/folder',
+    name: 'home',
+    component: Folder
   }
 ]
 
---
+++ src/views/Folder.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="margins-content">
+      <h1>Options</h1>
+      <p>folder location:</p>
+      <b-input-group prepend="Username" class="mt-3">
+        <b-form-input></b-form-input>
+        <b-input-group-append>
+          <b-button>Choose</b-button>
+        </b-input-group-append>
+      </b-input-group>
+      <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+    </div>
+    <div class="footer">
+      <hr />
+      <div class="d-flex margins-buttons">
+        <b-button variant="danger" class="mr-auto">cancel</b-button>
+        <b-button variant="primary">Install</b-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import '@/styles/installer.css'
+export default {
+
+}
+</script>
